683278b3cf17e4bbe6a3f7e15f1d299eb0fe6314
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-13 05:05:37 -0800

    WIP.

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -18,24 +18,17 @@ class MeshDataset(Dataset):
         self.file_list = os.listdir(folder_path)
 
     @staticmethod
-    def compare_faces(face_a, face_b, axis_order, vertices):
+    def compare_faces(face_a, face_b, vertices):
         for i in range(3):
             # Check if face indices are within the range of vertices list
             if face_a[i] >= len(vertices) or face_b[i] >= len(vertices):
                 raise IndexError("Face index out of range")
 
             vertex_comparison = MeshDataset.compare_vertices(
-                vertices[face_a[i]], vertices[face_b[i]], axis_order
+                vertices[face_a[i]], vertices[face_b[i]]
             )
             if vertex_comparison != 0:
                 return vertex_comparison
-
-        for i in range(3):
-            if face_a[i] < face_b[i]:
-                return -1
-            elif face_a[i] > face_b[i]:
-                return 1
-
         return 0
 
     def filter_files(self):
@@ -77,16 +70,22 @@ class MeshDataset(Dataset):
         return True
 
     @staticmethod
-    def compare_vertices(vertex_a, vertex_b, axis_order):
-        # Compare vertices according to the defined axis order
-        for axis in axis_order:
-            if vertex_a[axis] < vertex_b[axis]:
-                return -1
-            elif vertex_a[axis] > vertex_b[axis]:
-                return 1
-
+    def compare_vertices(vertex_a, vertex_b):
+        # glTF uses right-handed coordinate system (Y-Z-X).
+        # Y is up and is different from the meshgpt paper.
+        if vertex_a[1] < vertex_b[1]:
+            return -1
+        elif vertex_a[1] > vertex_b[1]:
+            return 1
+        elif vertex_a[2] < vertex_b[2]:
+            return -1
+        elif vertex_a[2] > vertex_b[2]:
+            return 1
+        elif vertex_a[0] < vertex_b[0]:
+            return -1
+        elif vertex_a[0] > vertex_b[0]:
+            return 1
         return 0
-
 
     def filter_files(self):
         filtered_list = [
@@ -99,24 +98,41 @@ class MeshDataset(Dataset):
     def __len__(self):
         return len(self.filter_files())
 
+    def transform_axis_order(self, data, src_format, dest_format):
+        axis_orders_to_gltf = {
+            ".ply": [2, 1, 0],  # Assuming Y-Z-X
+            ".stl": [0, 2, 1],  # Assuming X-Z-Y
+            ".obj": [0, 2, 1],  # X-Y-Z
+            ".glb": [0, 1, 2],  # X-Y-Z
+            ".gltf": [0, 1, 2],  # X-Y-Z
+        }
+        src_order = axis_orders_to_gltf[src_format]
+
+        return [data[i] for i in src_order]
+
+    def convert_to_dest_axis(self, source_extension, vertex_position):
+        forward_axis_conversion = {
+            ".ply": 1,
+            ".stl": -1,
+            ".obj": 1,
+            ".glb": 1,
+            ".gltf": 1,
+        }
+
+        # Transform the axis order from source to destination
+        dest_converted_vertex_position = self.transform_axis_order(np.array(vertex_position), source_extension, ".gltf")
+        
+        # Perform the operation on both lists
+        dest_converted_vertex_position[2] *= forward_axis_conversion[source_extension]
+
+        return dest_converted_vertex_position
+
     def __getitem__(self, idx):
         files = self.filter_files()
         file_path = os.path.join(self.folder_path, files[idx])
         _, file_extension = os.path.splitext(file_path)
 
-        axis_orders = {
-            '.ply': [1, 2, 0],  # Y-Z-X
-            '.stl': [0, 1, 2],  # X-Y-Z
-            '.obj': [0, 2, 1],  # X-Z-Y
-            '.glb': [1, 2, 0],  # Y-Z-X
-            '.gltf': [1, 2, 0]  # Y-Z-X
-        }
-
-        # Get the axis order for the current file type
-        axis_order = axis_orders.get(file_extension, [0, 1, 2])
-
         scene = trimesh.load(file_path, force="scene")
-        vertex_indices = {}
 
         all_triangles = []
         all_faces = []
@@ -130,13 +146,17 @@ class MeshDataset(Dataset):
             except Exception as e:
                 pass
 
-            vertices = [tuple(vertex) for vertex in geometry.vertices]
+            vertices = []
+            for vertex in geometry.vertices:
+                converted_vertex = self.convert_to_dest_axis(file_extension, vertex)
+                vertices.append(converted_vertex)
+            vertices = [tuple(v) for v in vertices]
             vertex_indices.update({v: i for i, v in enumerate(vertices)})
-
+            
             geometry.vertices = np.array(vertices)
-
+            
             offset = len(all_vertices)
-
+            
             faces = [
                 [
                     vertex_indices[tuple(geometry.vertices[vertex])] + offset
@@ -144,29 +164,43 @@ class MeshDataset(Dataset):
                 ]
                 for face in geometry.faces
             ]
-            faces = [[vertex for vertex in face] for face in faces]
-            geometry_data = (vertices, faces)
+          
+            # Calculate the normal of the first face
+            v1 = np.array(vertices[1]) - np.array(vertices[0])
+            v2 = np.array(vertices[2]) - np.array(vertices[0])
+            normal = np.cross(v1, v2)
 
-            triangles = [tuple(vertex) for vertex in geometry.vertices]
-            all_triangles.extend(triangles)
+            # If the z-coordinate of the normal is positive, the winding order is CCW
+            # Otherwise, it's CW
+            if normal[2] > 0:
+                print("Clockwise")
+                faces = [face[::-1] for face in faces]
+            else:
+                print("Counter-clockwise")
+            
+            faces = [[vertex for vertex in face] for face in faces]
+
             all_faces.extend(faces)
             all_vertices.extend(vertices)
 
         all_faces.sort(
             key=functools.cmp_to_key(
-                lambda a, b: MeshDataset.compare_faces(a, b, axis_order, all_vertices)
+                lambda a, b: MeshDataset.compare_faces(a, b, all_vertices)
             )
         )
 
         new_vertices = []
         new_faces = []
+        vertex_map = {}
 
         for face in all_faces:
             new_face = []
             for vertex_index in face:
-                if all_vertices[vertex_index] not in new_vertices:
-                    new_vertices.append(all_vertices[vertex_index])
-                new_face.append(new_vertices.index(all_vertices[vertex_index]))
+                if vertex_index not in vertex_map:
+                    new_vertex = all_vertices[vertex_index]
+                    new_vertices.append(new_vertex)
+                    vertex_map[vertex_index] = len(new_vertices) - 1
+                new_face.append(vertex_map[vertex_index])
             new_faces.append(new_face)
 
         return (
@@ -174,12 +208,40 @@ class MeshDataset(Dataset):
             torch.tensor(new_faces, dtype=torch.long),
         )
 
+import unittest
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        self.main = MeshDataset("unit_test")  # Assuming the class name is Main
+
+    def test_convert_to_dest_axis(self):
+        # Test case for .ply extension
+        result = self.main.convert_to_dest_axis(".ply", [1, 2, 3])
+        self.assertEqual(result, [3, 2, 1])
+
+        # Test case for .stl extension
+        result = self.main.convert_to_dest_axis(".stl", [1, 2, 3])
+        self.assertEqual(result, [1, 3, -2])
+
+        # Test case for .obj extension
+        result = self.main.convert_to_dest_axis(".obj", [1, 2, 3])
+        self.assertEqual(result, [1, 3, 2])
+
+        # Test case for .glb extension
+        result = self.main.convert_to_dest_axis(".glb", [1, 2, 3])
+        self.assertEqual(result, [1, 2, 3])
+
+        # Test case for .gltf extension
+        result = self.main.convert_to_dest_axis(".gltf", [1, 2, 3])
+        self.assertEqual(result, [1, 2, 3])
 
 if __name__ == "__main__":
+
     dataset = MeshDataset("unit_test")
 
     mesh_00 = [tensor.tolist() for tensor in dataset.__getitem__(0)]
-    
+
     with open("unit_test/mesh_00.json", "wb") as f:
         f.write(json.dumps(mesh_00).encode())
 
@@ -195,3 +257,4 @@ if __name__ == "__main__":
             print(f"JSON data 00 and {str(i).zfill(2)} are the same.")
         else:
             print(f"JSON data 00 and {str(i).zfill(2)} are different.")
+    unittest.main()
