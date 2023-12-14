a49aecbf5ffc28e33bf06267b3b820db5d8b8090
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-13 16:59:41 -0800

    Train the autoencoder.

--- demo.py
+++ demo.py
@@ -38,7 +38,7 @@ run = wandb.init(
 )
 
 if True:
-    load_from_checkpoint = True
+    load_from_checkpoint = False
     checkpoint_path = "checkpoints/mesh-autoencoder.ckpt.20.pt"
     autoencoder = None
     if load_from_checkpoint and os.path.isfile(checkpoint_path):
@@ -76,7 +76,7 @@ if True:
     transformer = MeshTransformer(
         autoencoder,
         dim=512,
-        max_seq_len=768,
+        max_seq_len=3384,
     ).to(device)
 
     transformer_trainer = MeshTransformerTrainer(
