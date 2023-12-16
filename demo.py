fd5e86e40df9b8c425fadfb4d656abf4fdb6632d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-15 16:33:41 -0800

    Something broke. Restoring.

--- demo.py
+++ demo.py
@@ -25,7 +25,7 @@ run = wandb.init(
         "architecture": "MeshGPT",
         "dataset": dataset_directory,
         "data_augment": data_augment,
-        "autoencoder_train": 200,
+        "autoencoder_train": 300,
         "transformer_train": 375,
         "batch_size": 1,
         "grad_accum_every": 1,
@@ -81,39 +81,73 @@ transformer = MeshTransformer(
 
 transformer_trainer = MeshTransformerTrainer(
     transformer,
-    num_train_steps=2000, # Ignored?
+    num_train_steps=2000,  # Ignored?
     dataset=dataset,
     batch_size=wandb.config.batch_size,
     grad_accum_every=wandb.config.grad_accum_every,
     checkpoint_every=wandb.config.checkpoint_every,
-    warmup_steps=500, # Ignored?
+    warmup_steps=500,  # Ignored?
     learning_rate=wandb.config.transformer_learning_rate,
     use_wandb_tracking=True,
 )
 
 transformer_trainer.train(run.config.transformer_train)
 
-continuous_coors = transformer.generate()
+codes, continuous_coors = transformer.generate(return_codes=True)
 
-continuous_coors_list = continuous_coors.cpu().tolist()
+codes_list = codes.cpu().tolist()
 
 import json
+
+with open("output_codes.json", "w") as f:
+    json.dump(codes_list, f)
+
+continuous_coors_list = continuous_coors.cpu().tolist()
 
 with open("continuous_coors.json", "w") as f:
     json.dump(continuous_coors.tolist(), f)
 
-if False:
-    import json
-
-    with open("continuous_coors.json", "r") as f:
-        continuous_coors_list = json.load(f)
-
 flat_list = [item for sublist in continuous_coors_list for item in sublist]
 
 vertices = [vertex for sublist in flat_list for vertex in sublist]
-# print("Vertices:", vertices)
 
 faces = [[i, i + 1, i + 2] for i in range(0, len(vertices), 3)]
 
-# Assuming dataset is an instance of a class that has a method convert_to_glb
 dataset.convert_to_glb((vertices, faces), "output.glb")
+dataset.convert_to_obj((vertices, faces), "output.obj")
+
+
+def encode_to_pua(codes):
+    flat_codes = [
+        item for sublist in codes for subsublist in sublist for item in subsublist
+    ]
+    return "".join(chr(code + 0xF0000) for code in flat_codes)
+
+
+encoded_codes = encode_to_pua(codes_list)
+
+with open("output.obj", "r") as file:
+    obj_contents = file.read()
+
+new_data = [
+    [
+        {
+            "role": "system",
+            "content": "This assistant can understand 3D models using the meshgpt-pytorch Unicode plane 15 codebook for 16384 triangles and the .ply 3d format.",
+        },
+        {"role": "user", "content": f"{encoded_codes}"},
+        {"role": "assistant", "content": f"{obj_contents}"},
+    ]
+]
+data = []
+try:
+    with open("chatml.jsonl", "r", encoding="utf-8") as f:
+        data = [json.loads(line) for line in f]
+except FileNotFoundError:
+    print("The file 'chatml.jsonl' does not exist.")
+
+data = new_data + data
+
+with open("chatml.jsonl", "w", encoding="utf-8") as f:
+    for item in data:
+        f.write(json.dumps(item, ensure_ascii=False) + "\n")
