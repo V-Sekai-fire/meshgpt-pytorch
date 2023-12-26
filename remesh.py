66eacb10e34e4ed0c72eca7890a8ae0ddd87b2ed
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-26 12:09:44 -0800

    Optimize the code.

--- remesh.py
+++ remesh.py
@@ -1,11 +1,11 @@
+import argparse
 import os
-import subprocess
 from multiprocessing import Pool
-import trimesh
-import numpy as np
 
 
 def run_command(cmd):
+    import subprocess
+
     process = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
@@ -15,29 +15,47 @@ def run_command(cmd):
     print(f"Stderr: {stderr.decode('utf-8')}")
     return stdout, stderr
 
-import argparse
 
-def process_glb_file(glb_path, output_glb_path):
-    output_basename = os.path.splitext(os.path.basename(glb_path))[0]
-    mesh = trimesh.load(glb_path, force="mesh")
+def load_mesh_process_export(file_path, output_path, file_type):
+    import trimesh
+
+    mesh = trimesh.load(file_path, force="mesh")
     mesh.merge_vertices(4)
     mesh.vertex_normals
-    source_file = os.path.normpath('temporary/' + output_basename + '.obj')
-    mesh.export(source_file, file_type="obj")
+    mesh.export(output_path, file_type=file_type)
+
+
+def process_glb_file(glb_path, output_glb_path):
+    import numpy as np
+
+    output_basename = os.path.splitext(os.path.basename(glb_path))[0]
+    temp_dir = "temporary"
+    source_file = os.path.normpath(os.path.join(temp_dir, f"{output_basename}.obj"))
+    load_mesh_process_export(glb_path, source_file, "obj")
+
     target_quad_count = 1000
-    source_file_second_stage = os.path.normpath("./temporary/" + output_basename + "_rem_p0.obj")
+    source_file_second_stage = os.path.normpath(
+        os.path.join(temp_dir, f"{output_basename}_rem_p0.obj")
+    )
     commands = [
-        os.path.normpath(f"./thirdparty/quadwild_windows/quadwild {source_file} 2 thirdparty/quadwild_windows/config/prep_config/basic_setup_Mechanical.txt"),
-        os.path.normpath(f"./thirdparty/quadwild_windows/quad_from_patches {source_file_second_stage} {target_quad_count} thirdparty/quadwild_windows/config/main_config/flow.txt"),
+        os.path.normpath(
+            f"./thirdparty/quadwild_windows/quadwild {source_file} 2 thirdparty/quadwild_windows/config/prep_config/basic_setup_Mechanical.txt"
+        ),
+        os.path.normpath(
+            f"./thirdparty/quadwild_windows/quad_from_patches {source_file_second_stage} {target_quad_count} thirdparty/quadwild_windows/config/main_config/flow.txt"
+        ),
     ]
     with Pool(os.cpu_count()) as p:
         results = p.map(run_command, commands)
 
-    remeshed_quadrangulation_smooth_obj_path = "temporary/" + output_basename + f"_rem_p0_{target_quad_count}_quadrangulation_smooth.obj"
-    mesh = trimesh.load(remeshed_quadrangulation_smooth_obj_path, force="mesh")
-    mesh.merge_vertices(4)
-    mesh.vertex_normals
-    mesh.export(output_glb_path, file_type="glb")
+    remeshed_quadrangulation_smooth_obj_path = os.path.join(
+        temp_dir,
+        f"{output_basename}_rem_p0_{target_quad_count}_quadrangulation_smooth.obj",
+    )
+    load_mesh_process_export(
+        remeshed_quadrangulation_smooth_obj_path, output_glb_path, "glb"
+    )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process a GLB file.")
