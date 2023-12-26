16f7f036c50bb47cb910899155a169ec3784223c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-25 21:31:47 -0800

    Update default learning rate docs for the autoencoder

--- run.py
+++ run.py
@@ -203,7 +203,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_augment", type=int, default=2, 
                         help="Number of data augmentations to apply. Default is 2.")
     parser.add_argument("--autoencoder_learning_rate", type=float, default=0.2, 
-                        help="Learning rate for the autoencoder. Default is 0.1.")
+                        help="Learning rate for the autoencoder. Default is 0.2.")
     parser.add_argument("--transformer_learning_rate", type=float, default=0.2, 
                         help="Learning rate for the transformer. Default is 0.2.")
     parser.add_argument("--autoencoder_train", type=int, default=600, 
