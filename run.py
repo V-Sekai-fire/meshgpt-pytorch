a122784625c42d93e0713fe849b97e8705b2db74
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-27 09:22:48 -0800

    Refactor run.py for improved readability and maintainability

--- run.py
+++ run.py
@@ -16,7 +16,13 @@ from sklearn.cluster import KMeans
 import unittest
 from scipy.spatial import KDTree
 from meshgpt_pytorch.data import MeshDataset
-from mesh_utils import generate_mesh_data, process_mesh_data, load_and_process_files, train_autoencoder, train_transformer
+from mesh_utils import (
+    generate_mesh_data,
+    process_mesh_data,
+    load_and_process_files,
+    train_autoencoder,
+    train_transformer,
+)
 
 
 if __name__ == "__main__":
